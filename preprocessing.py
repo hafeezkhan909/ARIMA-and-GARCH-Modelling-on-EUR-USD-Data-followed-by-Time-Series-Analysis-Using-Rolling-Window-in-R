7e8834e515a503ea160c0c887eaa363eb73e1254
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2022-12-21 21:02:31 +0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,6 @@
 import pandas as pd
 dataset = pd.read_csv('EURUSD.csv') #Reading the values from the dataset(csv file)
 dataset.info()
-
 '''
 RangeIndex: 3345 entries, 0 to 3344
 Data columns (total 2 columns):
@@ -12,3 +11,18 @@ Data columns (total 2 columns):
 dtypes: float64(1), object(1)
 memory usage: 52.4+ KB
 '''
+dataset = dataset.fillna(limit=5, limit_direction="backward");
+dataset = dataset.fillna(dataset.rolling(5, min_periods=1).mean().shift())
+#print(df5)
+dataset.info()
+'''
+RangeIndex: 3345 entries, 0 to 3344
+Data columns (total 2 columns):
+ #   Column  Non-Null Count  Dtype  
+---  ------  --------------  -----  
+ 0   Date    3345 non-null   object 
+ 1   Price   3345 non-null   float64
+dtypes: float64(1), object(1)
+memory usage: 52.4+ KB
+'''
+dataset.to_csv('DATA.csv', index=False)
